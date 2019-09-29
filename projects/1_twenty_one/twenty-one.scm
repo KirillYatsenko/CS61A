9ea37daf9c3bdbd03826c2287b7b5446a3c751b6
Kirill Yatsenko <kiriyatsenko@gmail.com>
2019-09-29 23:26:22 +0300

    play-n: function implemented

--- projects/1_twenty_one/twenty-one.scm
+++ projects/1_twenty_one/twenty-one.scm
@@ -2,7 +2,7 @@
 
 	(define (find-max-total totals current-max)
 		(cond ((empty? totals) current-max)
-		      ((> (first totals) current-max) (find-max-total (butfirst totals) (first totals)))	
+		      ( (and (> (first totals) current-max) (<= (first totals) 21))   (find-max-total (butfirst totals) (first totals)))	
 		      (else (find-max-total (butfirst totals) current-max))))
 
 	(define (all-total cards)
@@ -20,7 +20,8 @@
 		(define (card-value-helper card value)
 				(cond ((empty? card) value)
 				      ((or(member? (first card) '123456789) (equal? (first card) '0)) 
-						(card-value-helper (butfirst card) (word value (first card))))	      
+						(card-value-helper (butfirst card) (word value (first card))))
+				      ((member? (first card) 'jqk) 10)    
 				(else value)))
 		
 		(define (test-sentence sente count)
@@ -28,7 +29,6 @@
 			      (else (test-sentence (se sente 'ab) (- count 1)))))
 			
 		(cond 
-		      ((> total 21) 0 )
 		      ((empty? cards) total )
 		      ((not (is-ace (first cards)))
 		      	(se totals (all-total-help (butfirst cards) (+ total (card-value (first cards))) totals )))    
@@ -36,13 +36,24 @@
 			(se totals (all-total-help (butfirst cards) (+ total 1) totals) 
 				   (all-total-help (butfirst cards) (+ total 11) totals)))))
 
-	(find-max-total (all-total cards) 0))
+	(find-max-total (all-total cards) (first (all-total cards)) ))
 
 
-(define (stop-at-17 hand)
-	   (cond ((>= hand 17) #f)
+(define (stop-at-17 customer-hand-so-far dealer-up-card rest-of-deck)
+	   (cond ((>= (best-total customer-hand-so-far) 17) #f)
 	   (else #t)))
 
+(define (play-n strategy n)
+	(define (play-n-helper strategy n count win loss )
+		(let ((twenty-one-result (twenty-one strategy)))
+			(cond ((equal? count n) (- win loss) )
+			      ((equal? twenty-one-result -1 ) (play-n-helper strategy n (+ count 1) win (+ loss 1)))
+			      ((equal? twenty-one-result 0  ) (play-n-helper strategy n (+ count 1) win loss      ))
+			      ((equal? twenty-one-result 1  ) (play-n-helper strategy n (+ count 1) (+ win 1) loss)))))
+
+	(play-n-helper strategy n 0 0 0))
+			
+				
 (define (twenty-one strategy)
   (define (play-dealer customer-hand dealer-hand-so-far rest-of-deck)
     (cond ((> (best-total dealer-hand-so-far) 21) 1)
@@ -56,7 +67,7 @@
 
   (define (play-customer customer-hand-so-far dealer-up-card rest-of-deck)
     (cond ((> (best-total customer-hand-so-far) 21) -1)
-	  ((strategy customer-hand-so-far dealer-up-card)
+	  ((strategy customer-hand-so-far dealer-up-card rest-of-deck)
 	   (play-customer (se customer-hand-so-far (first rest-of-deck))
 			  dealer-up-card
 			  (bf rest-of-deck)))
